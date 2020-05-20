de27936baa56796d677ac0616a3b254e090a3a6a
Frank Hale <frank.hale@iptassociates.com>
2020-05-19 21:52:51 -0400

    can't make this work =(

--- API.Integration.Tests/API1Tests.cs
+++ API.Integration.Tests/API1Tests.cs
@@ -21,23 +21,23 @@ namespace API1.Integration.Tests
             _client = _factory.CreateClient(new WebApplicationFactoryClientOptions
             {
                 AllowAutoRedirect = true
-            });            
+            });
         }
 
         [Fact]
         public async Task Get_GetDefaultEndPoint_ReturnsOk()
         {
-            // Setting up the MassTransit Test Harness looks ok but I still have the issue
-            // with setting up the WebApplicationFactory (see TestSetup.cs for more details)
-            // https://masstransit-project.com/usage/testing.html#test-harness
-
-            // Arrange
             var harness = new InMemoryTestHarness();
-            var consumerHarness = harness.Consumer<MyMessageConsumer>();
+            var consumerHarness = harness.Consumer<MyMessageConsumer>("mymessage-endpoint");
 
             await harness.Start();
             try
             {
+                // Setting up the MassTransit Test Harness looks ok but I still have the issue
+                // with setting up the WebApplicationFactory (see TestSetup.cs for more details)
+                // https://masstransit-project.com/usage/testing.html#test-harness
+
+                // Arrange
                 // Act
                 var apiResults = await _client.GetAsync("/api/default");
                 var apiResponse = await apiResults.Content.ReadAsStringAsync();
@@ -46,10 +46,7 @@ namespace API1.Integration.Tests
                 apiResults.StatusCode.Should().Be(HttpStatusCode.OK);
                 apiResponse.Should().Be("API1");
 
-                // did the actual consumer consume the message
-                //Assert.IsTrue(consumerHarness.Consumed.Select<MyMessage>().Any());
-                
-                consumerHarness.Consumed.Select<MyMessage>().Any().Should().BeTrue();
+                consumerHarness.Consumed.Select<MyMessage>().FirstOrDefault().Should().NotBeNull();
             }
             finally
             {
--- API.Integration.Tests/TestSetup.cs
+++ API.Integration.Tests/TestSetup.cs
@@ -46,22 +46,23 @@ namespace API1.Integration.Tests
 
                 // The help on Testing looks promising but I still don't know how to configure my WebApplicationFactory 
                 // https://masstransit-project.com/usage/testing.html#test-harness
-                services.AddHealthChecks();
-
+                                
                 services.AddMassTransit(x =>
                 {
                     x.AddConsumer<MyMessageConsumer>();
                     x.AddBus(provider => Bus.Factory.CreateUsingInMemory(cfg =>
                     {
-                        cfg.UseHealthCheck(provider);
                         cfg.ReceiveEndpoint("mymessage-endpoint", ep =>
-                        {
+                        {                            
+                            ep.UseMessageRetry(r => r.Interval(2, 100));
                             ep.ConfigureConsumer<MyMessageConsumer>(provider);
                         });
                     }));
                 });
                 services.AddMassTransitHostedService();
                 services.AddSingleton<IBus>(provider => provider.GetRequiredService<IBusControl>());
+
+                services.BuildServiceProvider();
             });
         }
     }
